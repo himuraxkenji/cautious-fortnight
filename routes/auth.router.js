fd9e60ba3ddce2edf6b00532dc5bb8e70905a778
himuraxkenji <aaron.ariperto@gmail.com>
2023-05-31 15:31:01 -0300

    reformat code of auth layer

--- routes/auth.router.js
+++ routes/auth.router.js
@@ -1,43 +1,45 @@
 const express = require('express');
 const passport = require('passport');
+
+const AuthService = require('./../services/auth.service');
+
 const router = express.Router();
-const AuthService = require('../services/auth.service');
-const service  = new AuthService();
+const service = new AuthService();
 
-router.post('/login',  
-  passport.authenticate('local', { session: false }),
+router.post('/login',
+  passport.authenticate('local', {session: false}),
   async (req, res, next) => {
-  try {
-    const user = req.user;
-    res.json(service.signToken(user))
-  } catch (error) {
-    next(error);
+    try {
+      const user = req.user;
+      res.json(service.signToken(user));
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-
-router.post('/recovery',  
-  passport.authenticate('local', { session: false }),
+router.post('/recovery',
   async (req, res, next) => {
-  try {
-    const {email} = req.body;
-    const rta = await service.sentRecovery(email)
-    res.json(rta)
-  } catch (error) {
-    next(error);
+    try {
+      const { email } = req.body;
+      const rta = await service.sendRecovery(email);
+      res.json(rta);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
-
-router.post('/change-password',  
+router.post('/change-password',
   async (req, res, next) => {
-  try {
-    const {token, newPassword} = req.body;
-    const rta = await service.changePassword(token, newPassword )
-    res.json(rta)
-  } catch (error) {
-    next(error);
+    try {
+      const { token, newPassword } = req.body;
+      const rta = await service.changePassword(token, newPassword);
+      res.json(rta);
+    } catch (error) {
+      next(error);
+    }
   }
-});
+);
 
 module.exports = router;
